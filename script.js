944bdb7d6b6e3c79532c8da3a5bc491f5a9e3ac7
ewio <44951495+notewio@users.noreply.github.com>
2023-01-04 12:54:12 -0500

    qol
    im not on my usual computer, thats why im github bashing im terribly sorry,

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 1, 1);
+camera.position.set(0, 3, 3);
 
 // Grid floor
 scene.add(new THREE.GridHelper(20, 20));
